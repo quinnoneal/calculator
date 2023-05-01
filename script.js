3c2f4501a80e5cdf763e3ba0d8a41e3a348612b9
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-01 12:42:41 -1000

    add support for single calculation, add helper methods with calculations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -32,6 +32,32 @@ function updateDisplay(value) {
     display.textContent += value;
 }
 
+function isOperator(value) {
+    switch(value) {
+        case '+':
+            operator = '+';
+            return true;
+        case '-':
+            operator = '-';
+            return true;
+        case '*':
+            operator = '*';
+            return true;
+        case '/':
+            operator = '/';
+            return true;
+        default:
+            return false;
+    }
+}
+
+function clearDisplay() {
+    display.textContent = '';
+    a = 0;
+    b = 0;
+    operator = null;
+}
+
 let a;
 let operator;
 let b;
@@ -42,7 +68,26 @@ const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        updateDisplay(button.textContent);
+        let buttonPressed = button.textContent;
+        // check if user clicked operator, if so, if operator variable != null, operate, then show sum plus operator pressed.
+        if(isOperator(buttonPressed)) {
+            // add space before and after operator for better look and to split elements when = is pressed.
+            display.textContent += ' ';
+            updateDisplay(buttonPressed);
+            display.textContent += ' ';
+        } else if(buttonPressed === '=') {
+            // get second operand
+            // reset operator variable
+            let splitEquation = display.textContent.split(' ');
+            operator = splitEquation[1];
+            a = splitEquation[0];
+            b = splitEquation[2];
+            display.textContent = operate(operator, a, b);
+        } else if(buttonPressed === 'Clear') {
+            clearDisplay();
+        } else {
+            updateDisplay(buttonPressed);
+        }
     });
 })
 
