68a993ab6637ae87e8d2cf1a8b3addf6a51d20ad
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-03 15:28:25 -1000

    calculator is fully functional

--- script.js
+++ script.js
@@ -63,12 +63,14 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', () => {
         let buttonPressed = button.textContent;
         // check if user clicked operator, if so, if operator variable != null, operate, then show sum plus operator pressed.
         if(isOperator(buttonPressed)) {
+            if(display.textContent === '') {
+
             // check if operator already exists
-            if(operator === undefined) {
+            } else if(operator === undefined) {
                 // add space before and after operator for better look and to split elements when = is pressed.
                 operator = buttonPressed;
                 display.textContent += ' ';
@@ -85,27 +87,32 @@ buttons.forEach((button) => {
                 updateDisplay(buttonPressed);
                 display.textContent += ' ';    
             } else {
-                console.log(operator)
-                console.log(display.textContent)
                 display.textContent = display.textContent.replace(operator, buttonPressed);
                 operator = buttonPressed;
             }
-        } else if(buttonPressed === '=') {
-            // get second operand
-            // reset operator variable
-            let splitEquation = display.textContent.split(' ');
-            operator = splitEquation[1];
-            a = splitEquation[0];
-            b = splitEquation[2];
-            display.textContent = operate(operator, a, b);
-            a = undefined;
-            b = undefined;
-            operator = undefined;
-        } else if(buttonPressed === 'Clear') {
-            clearDisplay();
-        } else {
+            // TODO: add conditional to fix bug where answer doesn't clear when entering new numbers after operation
+        } else if(buttonPressed !== '='){
+            // user just entered a number
             updateDisplay(buttonPressed);
         }
     });
 })
 
+const clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', () => clearDisplay());
+
+const equalsButton = document.querySelector('#equals');
+
+equalsButton.addEventListener('click', () => {
+    let splitEquation = display.textContent.split(' ');
+    operator = splitEquation[1];
+    a = splitEquation[0];
+    b = splitEquation[2];
+    console.log(b);
+    display.textContent = operate(operator, a, b);
+    a = undefined;
+    b = undefined;
+    operator = undefined;
+
+})
